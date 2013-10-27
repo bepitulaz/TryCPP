5ba8fecced2eb962ee9958ec8f8a3f1e55524fe0
Asep Bagja <bepitulaz@gmail.com>
2013-10-27 21:19:13 +0700

    call cpp function from obj-c

--- TryCPP/CPViewController.mm
+++ TryCPP/CPViewController.mm
@@ -7,6 +7,7 @@
 //
 
 #import "CPViewController.h"
+#import "xcode.mm"
 
 @interface CPViewController ()
 
@@ -18,6 +19,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    callMe();
 }
 
 - (void)didReceiveMemoryWarning
---
+++ TryCPP/xcode.mm
@@ -0,0 +1,16 @@
+//
+//  xcode.mm
+//  TryCPP
+//
+//  Created by Asep Bagja on 10/27/13.
+//  Copyright (c) 2013 TryCPP. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+void callMe() {
+    cout << "Call from Obj-C" << endl;
+}
